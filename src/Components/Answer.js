07e3dd204c666238b2e43cc5fd565614240a17c5
Ivelisse Morales <imorales315@gmail.com>
2020-11-06 23:54:51 -0500

    Refactor

--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -1,14 +1,8 @@
-import { Modal } from 'react-bootstrap';
-
 function Answer(props) {
 
     return (
         <div>
-            <Modal.Dialog>
-                <Modal.Body>
-                    <p>{props.correctAnswer == props.choice ? 'Correct!' : `Incorrect! The correct answer was: ${props.correctAnswer}`}</p>
-                </Modal.Body>
-            </Modal.Dialog>
+            <p>{props.correctAnswer == props.choice ? 'Correct!' : `Incorrect! The correct answer was: ${props.correctAnswer}`}</p>
         </div>
     );
 }
